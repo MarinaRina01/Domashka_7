7c5915c4700384bd8e57419f82b9e6166634b017
Marina <marina.shubochkina@gmail.com>
2023-04-20 00:06:48 +0300

    Save zd3

---
+++ zd3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int rows = 3;
+int colomns = 4;
+int[,] matrix = GetRandomMatrix(rows, colomns, 0, 9);
+PrintMatrix(matrix);
+GetAvg(matrix);
+
+int[,] GetRandomMatrix(int rows, int colomns, int min, int max)
+{
+    int[,] array = new int[rows, colomns];
+    var rand = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return array;
+
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void GetAvg(int[,] matrix)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        Console.Write($"{Math.Round(sum / matrix.GetLength(0), 1)} ");
+    }
+}
+
